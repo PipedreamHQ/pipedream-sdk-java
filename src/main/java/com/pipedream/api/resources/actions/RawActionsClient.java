8b033b57835e60aaa0aa29214d91131d7129cfd4
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-07 16:15:15 +0000

    SDK regeneration

--- src/main/java/com/pipedream/api/resources/actions/RawActionsClient.java
+++ src/main/java/com/pipedream/api/resources/actions/RawActionsClient.java
@@ -275,9 +275,6 @@ public class RawActionsClient {
         if (request.getDynamicPropsId().isPresent()) {
             properties.put("dynamic_props_id", request.getDynamicPropsId());
         }
-        if (request.getStashId().isPresent()) {
-            properties.put("stash_id", request.getStashId());
-        }
         RequestBody body;
         try {
             body = RequestBody.create(
